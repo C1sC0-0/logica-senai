75a0696f55bccc17c0d5e3e723d1231657d210db
C1sC0-0 <guilhermefrancisco0711@gmail.com>
2025-05-22 16:16:59 -0300

    Add files via upload

---
+++ 6.Laco.js
@@ -0,0 +1,5 @@
+// Laço de repetição.
+
+for (let i = 1; i <= 10; i+=2) {
+    console.log(i)
+}
---
+++ 7.atv.js
@@ -0,0 +1,6 @@
+const readlineSync = require('readline-sync')
+
+let numero = parseInt(readlineSync.question("Digite um numero"))
+console.log(`Tabela de Numero ${Numero}`)
+
+for (let i = 1; i<= 10; i++)        
